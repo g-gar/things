883a557b23310a4c8003e208bf44dd99ff34a63b
Unknown <g.gar.fern@hotmail.com>
2017-10-06 09:48:29 +0200

    .

--- Converter.js
+++ Converter.js
@@ -1,22 +1,46 @@
 /*
  * Author: g-gar
- * Version: 1.0.0
+ * Version: 1.0.1
  */
 
 class Converter {
     constructor(){
         
     }
-    static convert(string, unit){
-        let units = void 0
-        for (let i in this.UNITS) if (this.UNITS[i].indexOf(unit) > -1) units = this.UNITS[i]
+    static basicConvert(quantity, unitFrom, unitTo){
+    	let units = void 0
+    	let unitFrom_position = void 0
+    	let unitTo_position = void 0
+
+    	for (let i in this.UNITS) {
+    		if ((unitFrom_position = this.UNITS[i].units.indexOf(unitFrom)) > -1 && (unitTo_position = this.UNITS[i].units.indexOf(unitTo)) > -1) {
+    			units = this.UNITS[i].units
+    			break
+    		} else unitFrom_position, unitTo_position = void 0
+    	}
+    	console.log(unitFrom_position, unitTo_position)
+    	return units && unitFrom_position && unitTo_position ? quantity * Math.pow(10, unitFrom_position - unitTo_position) : units
+    }
+    static convertString(string, unitTo){
         let temp = string.split(' ')
-        return `${parseFloat(temp[0]) * Math.pow(10, units.indexOf(temp[1]) - units.indexOf(unit))}`
+        return this.convert(parseFloat(temp[0]), temp[1], unitTo) 
+    }
+    static convert(params, unitTo){
+    	let res = void 0
+    	if (typeof params == 'string') res = this.convertString(params, unitTo)
+
+    	return res
     }
     static get UNITS(){
         return {
-            mass: "mg cg dg g dag hg kg".split(' '),
-            length: "ym   zm   am X  fm  Å pm   nm   µm   mm cm dm m dam hm Km   Mm   Gm   Tm   Pm   Em   Zm   Ym".split(' ')
+            mass: {
+            	units: 'mg cg dg g dag hg kg'.split(' '),
+            	unit: 'g'
+            },
+            length: {
+            	units: 'ym   zm   am X  fm  Å pm   nm   µm   mm cm dm m dam hm Km   Mm   Gm   Tm   Pm   Em   Zm   Ym'.split(' '),
+            	unit: 'm'
+            }
         }
     }
 }
